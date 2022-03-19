fbeb95d8eb3f7f43b7eb8d42fe523f39aa07c778
danielmarthaler <daniel.marthaler@stitchfix.com>
2022-03-18 21:39:12 -0400

    workign to fix bugs in swing up

--- examples/pendulum_swing_up.py
+++ examples/pendulum_swing_up.py
@@ -1,93 +1,97 @@
-import numpy as np
+import jax.numpy as jnp
+import objax
 import gym
+from bayesnewton.utils import softplus_inv
 from pilco.models import PILCO
-from pilco.controllers import RbfController, LinearController
+from pilco.controllers import RbfController
 from pilco.rewards import ExponentialReward
-import tensorflow as tf
-from utils import rollout, policy
-from gpflow import set_trainable
-np.random.seed(0)
 
-# NEEDS a different initialisation than the one in gym (change the reset() method),
-# to (m_init, S_init), modifying the gym env
+from utils import rollout
 
-# Introduces subsampling with the parameter SUBS and modified rollout function
-# Introduces priors for better conditioning of the GP model
-# Uses restarts
+seed = 0
+objax.random.Generator(seed)
 
-class myPendulum():
-    def __init__(self):
-        self.env = gym.make('Pendulum-v0').env
-        self.action_space = self.env.action_space
-        self.observation_space = self.env.observation_space
 
-    def step(self, action):
-        return self.env.step(action)
-
-    def reset(self):
-        high = np.array([np.pi, 1])
-        self.env.state = np.random.uniform(low=-high, high=high)
-        self.env.state = np.random.uniform(low=0, high=0.01*high) # only difference
-        self.env.state[0] += -np.pi
-        self.env.last_u = None
-        return self.env._get_obs()
-
-    def render(self):
-        self.env.render()
-
-if __name__=='__main__':
-    SUBS=3
+if __name__ == "__main__":
+    SUBS = 3
     bf = 30
-    maxiter=50
-    max_action=2.0
-    target = np.array([1.0, 0.0, 0.0])
-    weights = np.diag([2.0, 2.0, 0.3])
-    m_init = np.reshape([-1.0, 0, 0.0], (1,3))
-    S_init = np.diag([0.01, 0.05, 0.01])
+    maxiter = 50
+    max_action = 2.0
+    target = jnp.array([1.0, 0.0, 0.0])
+    weights = jnp.diag(jnp.array([2.0, 2.0, 0.3]))
+    m_init = jnp.reshape(jnp.array([-1.0, 0, 0.0]), (1, 3))
+    S_init = jnp.diag(jnp.array([0.01, 0.05, 0.01]))
     T = 40
     T_sim = T
     J = 4
     N = 8
     restarts = 2
 
-    env = myPendulum()
+    env = gym.make("Pendulum-v1").env
 
     # Initial random rollouts to generate a dataset
     X, Y, _, _ = rollout(env, None, timesteps=T, random=True, SUBS=SUBS, render=True)
-    for i in range(1,J):
-        X_, Y_, _, _ = rollout(env, None, timesteps=T, random=True, SUBS=SUBS, verbose=True, render=True)
-        X = np.vstack((X, X_))
-        Y = np.vstack((Y, Y_))
+    for i in range(1, J):
+        X_, Y_, _, _ = rollout(
+            env, None, timesteps=T, random=True, SUBS=SUBS, verbose=True, render=True
+        )
+        X = jnp.vstack((X, X_))
+        Y = jnp.vstack((Y, Y_))
 
     state_dim = Y.shape[1]
     control_dim = X.shape[1] - state_dim
 
-    controller = RbfController(state_dim=state_dim, control_dim=control_dim, num_basis_functions=bf, max_action=max_action)
+    controller = RbfController(
+        state_dim=state_dim,
+        control_dim=control_dim,
+        num_basis_functions=bf,
+        max_action=max_action,
+    )
     R = ExponentialReward(state_dim=state_dim, t=target, W=weights)
 
-    pilco = PILCO((X, Y), controller=controller, horizon=T, reward=R, m_init=m_init, S_init=S_init)
+    pilco = PILCO(
+        (X, Y),
+        controller=controller,
+        horizon=T,
+        reward=R,
+        m_init=m_init,
+        S_init=S_init,
+        trainable_likelihood_variance=False,
+    )
 
-    # for numerical stability, we can set the likelihood variance parameters of the GP models
+    # for numerical stability, we can set the likelihood variance parameters
+    # of the GP models
     for model in pilco.mgpr.models:
-        model.likelihood.variance.assign(0.001)
-        set_trainable(model.likelihood.variance, False)
+        model.likelihood.transformed_variance.value = softplus_inv(jnp.array(0.001))
 
-    r_new = np.zeros((T, 1))
+    r_new = []
     for rollouts in range(N):
         print("**** ITERATION no", rollouts, " ****")
         pilco.optimize_models(maxiter=maxiter, restarts=2)
         pilco.optimize_policy(maxiter=maxiter, restarts=2)
-
-        X_new, Y_new, _, _ = rollout(env, pilco, timesteps=T_sim, verbose=True, SUBS=SUBS, render=True)
+        print("About to rollout")
+        # X_new, Y_new, _, _ = rollout(
+        #     env,
+        #     pilco,
+        #     timesteps=T_sim,
+        #     verbose=True,
+        #     random=False,
+        #     SUBS=SUBS,
+        #     render=True,
+        # )
+        X_new, Y_new, _, _ = rollout(env, pilco, timesteps=T_sim)
 
         # Since we had decide on the various parameters of the reward function
         # we might want to verify that it behaves as expected by inspection
-        for i in range(len(X_new)):
-                r_new[:, 0] = R.compute_reward(X_new[i,None,:-1], 0.001 * np.eye(state_dim))[0]
-        total_r = sum(r_new)
-        _, _, r = pilco.predict(X_new[0,None,:-1], 0.001 * S_init, T)
-        print("Total ", total_r, " Predicted: ", r)
+        # for i in range(len(X_new)):
+        #     r_new.append(
+        #         R.compute_reward(X_new[i, None, :-1], 0.001 * jnp.eye(state_dim))[0]
+        #     )
+        # total_r = sum(r_new)
+        # _, _, r = pilco.predict(X_new[0, None, :-1], 0.001 * S_init, T)
+        # print("Total ", total_r, " Predicted: ", r)
 
         # Update dataset
-        X = np.vstack((X, X_new)); Y = np.vstack((Y, Y_new))
+        X = jnp.vstack((X, X_new))
+        Y = jnp.vstack((Y, Y_new))
         pilco.mgpr.set_data((X, Y))
