6b7158100b164ce44a0e902e45be42fb30b89fab
danielmarthaler <daniel.marthaler@stitchfix.com>
2022-03-21 10:14:33 -0400

    added swimmer example

--- examples/swimmer.py
+++ examples/swimmer.py
@@ -11,7 +11,7 @@ seed = 0
 objax.random.Generator(seed)
 
 name = "swimmer_new" + str(seed)
-env = gym.make("Swimmer-v2").env
+env = gym.make("Swimmer-v3").env
 
 state_dim = 8
 control_dim = 2
@@ -29,37 +29,37 @@ T_sim = 50
 # Reward function that dicourages the joints from hitting their max angles
 max_ang = 95 / 180 * jnp.pi
 rewards = []
-rewards.append(LinearReward(state_dim, [0, 0, 0, 1.0, 0, 0, 0, 0]))
+rewards.append(LinearReward(state_dim, jnp.array([0, 0, 0, 1.0, 0, 0, 0, 0])))
 rewards.append(
     ExponentialReward(
         state_dim,
         W=jnp.diag(jnp.array([0, 0, 10, 0, 0, 0, 0, 0]) + 1e-6),
-        t=[0, 0, max_ang, 0, 0, 0, 0, 0],
+        t=jnp.array([0, 0, max_ang, 0, 0, 0, 0, 0]),
     )
 )
 rewards.append(
     ExponentialReward(
         state_dim,
         W=jnp.diag(jnp.array([0, 0, 10, 0, 0, 0, 0, 0]) + 1e-6),
-        t=[0, 0, -max_ang, 0, 0, 0, 0, 0],
+        t=jnp.array([0, 0, -max_ang, 0, 0, 0, 0, 0]),
     )
 )
 rewards.append(
     ExponentialReward(
         state_dim,
         W=jnp.diag(jnp.array([0, 10, 0, 0, 0, 0, 0, 0]) + 1e-6),
-        t=[0, max_ang, 0, 0, 0, 0, 0, 0],
+        t=jnp.array([0, max_ang, 0, 0, 0, 0, 0, 0]),
     )
 )
 rewards.append(
     ExponentialReward(
         state_dim,
         W=jnp.diag(jnp.array([0, 10, 0, 0, 0, 0, 0, 0]) + 1e-6),
-        t=[0, -max_ang, 0, 0, 0, 0, 0, 0],
+        t=jnp.array([0, -max_ang, 0, 0, 0, 0, 0, 0]),
     )
 )
 combined_reward = CombinedRewards(
-    state_dim, rewards, coefs=[1.0, -1.0, -1.0, -1.0, -1.0]
+    state_dim, rewards, coefs=jnp.array([1.0, -1.0, -1.0, -1.0, -1.0])
 )
 # Initial random rollouts to generate a dataset
 X, Y, _, _ = rollout(
@@ -92,8 +92,8 @@ pilco = PILCO(
 
 logging = False  # To save results in .csv files turn this flag to True
 eval_runs = 10
-# evaluation_returns_full = jnp.zeros((N, eval_runs))
-# evaluation_returns_sampled = jnp.zeros((N, eval_runs))
+evaluation_returns_full = []
+evaluation_returns_sampled = []
 eval_max_timesteps = 1000 // SUBS
 X_eval = False
 for rollouts in range(N):
@@ -105,16 +105,16 @@ for rollouts in range(N):
         env, pilco, timesteps=T_sim, verbose=True, SUBS=SUBS, render=True
     )
 
-    cur_rew = 0
-    for t in range(0, len(X_new)):
-        cur_rew += R.compute_reward(
-            X_new[t, 0:state_dim, None].transpose(), 0.0001 * jnp.eye(state_dim)
-        )[0]
-        if t == T:
-            print(
-                "On this episode, on the planning horizon, PILCO reward was: ", cur_rew
-            )
-    print("On this episode PILCO reward was ", cur_rew)
+    # cur_rew = 0
+    # for t in range(0, len(X_new)):
+    #     cur_rew += pilco.compute_reward(
+    #         X_new[t, 0:state_dim, None].transpose(), 0.0001 * jnp.eye(state_dim)
+    #     )[0]
+    #     if t == T:
+    #         print(
+    #             f"On episode {rollouts}, on the planning horizon {T}, PILCO reward was: {cur_rew}"
+    #         )
+    # print("On this episode PILCO reward was ", cur_rew)
 
     gym_steps = 1000
     T_eval = gym_steps // SUBS
@@ -125,13 +125,10 @@ for rollouts in range(N):
     if logging:
         if eval_max_timesteps is None:
             eval_max_timesteps = sim_timesteps
+        evaluation_returns_sampled_temp = []
+        evaluation_returns_full_temp = []
         for k in range(0, eval_runs):
-            [
-                X_eval_,
-                _,
-                evaluation_returns_sampled[rollouts, k],
-                evaluation_returns_full[rollouts, k],
-            ] = rollout(
+            [X_eval_, _, a, b,] = rollout(
                 env,
                 pilco,
                 timesteps=eval_max_timesteps,
@@ -139,10 +136,14 @@ for rollouts in range(N):
                 SUBS=SUBS,
                 render=False,
             )
+            evaluation_returns_sampled_temp.append(a)
+            evaluation_returns_full_temp.append(b)
             if not X_eval:
                 X_eval = X_eval_.copy()
             else:
-                X_eval = np.vstack((X_eval, X_eval_))
+                X_eval = jnp.vstack((X_eval, X_eval_))
+        evaluation_returns_sampled.append(evaluation_returns_sampled_temp)
+        evaluation_returns_full.append(evaluation_returns_full_temp)
         # np.savetxt("X_" + name + seed + ".csv", X, delimiter=",")
         # np.savetxt("X_eval_" + name + seed + ".csv", X_eval, delimiter=",")
         # np.savetxt(
