3fc7c42c9f8522e9d7b0323ee9bf161ac96dc998
danielmarthaler <daniel.marthaler@stitchfix.com>
2022-05-02 12:52:36 -0400

    first pass for safe cars

--- examples/safe_cars_run.py
+++ examples/safe_cars_run.py
@@ -1,20 +1,15 @@
-import numpy as np
-import tensorflow as tf
-import gym
-from pilco.models import PILCO
-from pilco.controllers import RbfController, LinearController
-from pilco.rewards import ExponentialReward, LinearReward
+import jax.numpy as jnp
+import objax
+
+from pilco.controllers import RbfController
+from pilco.rewards import LinearReward
 
 from linear_cars_env import LinearCars
 
-from safe_pilco_extension.rewards_safe import RiskOfCollision, ObjectiveFunction
-from safe_pilco_extension.safe_pilco import SafePILCO
-from utils import rollout, policy
+from safe_pilco_extension.rewards_safe import RiskOfCollision
 
-from gpflow import config
-from gpflow import set_trainable
-int_type = config.default_int()
-float_type = config.default_float()
+from safe_pilco_extension.safe_pilco import SafePILCO
+from utils import rollout
 
 
 class Normalised_Env():
@@ -26,14 +21,14 @@ class Normalised_Env():
         self.std = std
 
     def state_trans(self, x):
-        return np.divide(x-self.m, self.std)
+        return jnp.divide(x-self.m, self.std)
 
     def step(self, action):
         ob, r, done, _ = self.env.step(action)
         return self.state_trans(ob), r, done, {}
 
     def reset(self):
-        ob =  self.env.reset()
+        ob = self.env.reset()
         return self.state_trans(ob)
 
     def render(self):
@@ -43,30 +38,58 @@ class Normalised_Env():
 def safe_cars(seed=0):
     T = 25
     th = 0.10
-    np.random.seed(seed)
+    objax.random.Generator(seed)
     J = 5
     N = 5
     eval_runs = 5
     env = LinearCars()
     # Initial random rollouts to generate a dataset
-    X1, Y1, _, _ = rollout(env, pilco=None, timesteps=T, verbose=True, random=True, render=False)
-    for i in range(1,5):
-        X1_, Y1_, _, _ = rollout(env, pilco=None, timesteps=T, verbose=True, random=True, render=False)
-        X1 = np.vstack((X1, X1_))
-        Y1 = np.vstack((Y1, Y1_))
+    X1, Y1, _, _ = rollout(
+        env,
+        pilco=None,
+        timesteps=T,
+        verbose=True,
+        random=True,
+        render=False
+    )
+    for i in range(1, 5):
+        X1_, Y1_, _, _ = rollout(
+            env,
+            pilco=None,
+            timesteps=T,
+            verbose=True,
+            random=True,
+            render=False
+        )
+        X1 = jnp.vstack((X1, X1_))
+        Y1 = jnp.vstack((Y1, Y1_))
 
-    env = Normalised_Env(np.mean(X1[:,:4],0), np.std(X1[:,:4], 0))
-    X, Y, _, _ = rollout(env, pilco=None, timesteps=T, verbose=True, random=True, render=False)
-    for i in range(1,J):
-        X_, Y_, _, _ = rollout(env, pilco=None, timesteps=T, verbose=True, random=True, render=False)
-        X = np.vstack((X, X_))
-        Y = np.vstack((Y, Y_))
+    env = Normalised_Env(jnp.mean(X1[:, :4], 0), jnp.std(X1[:, :4], 0))
+    X, Y, _, _ = rollout(
+        env,
+        pilco=None,
+        timesteps=T,
+        verbose=True,
+        random=True,
+        render=False
+    )
+    for i in range(1, J):
+        X_, Y_, _, _ = rollout(
+            env,
+            pilco=None,
+            timesteps=T,
+            verbose=True,
+            random=True,
+            render=False
+        )
+        X = jnp.vstack((X, X_))
+        Y = jnp.vstack((Y, Y_))
 
     state_dim = Y.shape[1]
     control_dim = X.shape[1] - state_dim
 
-    m_init = np.transpose(X[0,:-1,None])
-    S_init = 0.1 * np.eye(state_dim)
+    m_init = jnp.transpose(X[0, :-1, None])
+    S_init = 0.1 * jnp.eye(state_dim)
 
     controller = RbfController(state_dim=state_dim, control_dim=control_dim,
                                num_basis_functions=40, max_action=0.2)
@@ -75,25 +98,40 @@ def safe_cars(seed=0):
     #t1 = np.divide(np.array([3.0, 1.0, 3.0, 1.0]) - env.m, env.std)
     #R1 = ExponentialReward(state_dim=state_dim, t=t1, W=w1)
     # R1 = LinearReward(state_dim=state_dim, W=np.array([0.1, 0.0, 0.0, 0.0]))
-    R1 = LinearReward(state_dim=state_dim, W=np.array([1.0 * env.std[0], 0., 0., 0,]))
+    R1 = LinearReward(
+        state_dim=state_dim,
+        W=jnp.array([1.0 * env.std[0], 0., 0., 0])
+    )
 
     bound_x1 = 1 / env.std[0]
     bound_x2 = 1 / env.std[2]
-    B = RiskOfCollision(2, [-bound_x1-env.m[0]/env.std[0], -bound_x2 - env.m[2]/env.std[2]],
-                           [bound_x1 - env.m[0]/env.std[0], bound_x2 - env.m[2]/env.std[2]])
+    B = RiskOfCollision(
+        2,
+        [-bound_x1-env.m[0]/env.std[0], -bound_x2 - env.m[2] / env.std[2]],
+        [bound_x1 - env.m[0]/env.std[0], bound_x2 - env.m[2] / env.std[2]]
+    )
 
-    pilco = SafePILCO((X, Y), controller=controller, mu=-300.0, reward_add=R1, reward_mult=B, horizon=T, m_init=m_init, S_init=S_init)
+    pilco = SafePILCO(
+        (X, Y),
+        controller=controller,
+        mu=-300.0,
+        reward_add=R1,
+        reward_mult=B,
+        horizon=T,
+        m_init=m_init,
+        S_init=S_init,
+        trainable_likelihood_variance=False
+    )
 
     for model in pilco.mgpr.models:
         model.likelihood.variance.assign(0.001)
-        set_trainable(model.likelihood.variance, False)
 
     # define tolerance
     new_data = True
     # init = tf.global_variables_initializer()
-    evaluation_returns_full = np.zeros((N, eval_runs))
-    evaluation_returns_sampled = np.zeros((N, eval_runs))
-    X_eval = []
+    #evaluation_returns_full = jnp.zeros((N, eval_runs))
+    #evaluation_returns_sampled = jnp.zeros((N, eval_runs))
+    #X_eval = []
     for rollouts in range(N):
         print("***ITERATION**** ", rollouts)
         if new_data:
@@ -101,42 +139,57 @@ def safe_cars(seed=0):
             new_data = False
         pilco.optimize_policy(maxiter=20, restarts=2)
         # check safety
-        m_p = np.zeros((T, state_dim))
-        S_p = np.zeros((T, state_dim, state_dim))
-        predicted_risks = np.zeros(T)
-        predicted_rewards = np.zeros(T)
+        m_p = jnp.zeros((T, state_dim))
+        S_p = jnp.zeros((T, state_dim, state_dim))
+        predicted_risks = jnp.zeros(T)
+        predicted_rewards = jnp.zeros(T)
 
         for h in range(T):
             m_h, S_h, _ = pilco.predict(m_init, S_init, h)
-            m_p[h,:], S_p[h,:,:] = m_h[:], S_h[:,:]
+            m_p[h, :], S_p[h, :, :] = m_h[:], S_h[:, :]
             predicted_risks[h], _ = B.compute_reward(m_h, S_h)
             predicted_rewards[h], _ = R1.compute_reward(m_h, S_h)
-        overall_risk = 1 - np.prod(1.0-predicted_risks)
+        overall_risk = 1 - jnp.prod(1.0-predicted_risks)
 
         print("Predicted episode's return: ", sum(predicted_rewards))
         print("Overall risk ", overall_risk)
-        print("Mu is ", pilco.mu.numpy())
+        print("Mu is ", pilco.mu)
         print("bound1 ", bound_x1, " bound1 ", bound_x2)
 
         if overall_risk < th:
-            X_new, Y_new, _, _ = rollout(env, pilco=pilco, timesteps=T, verbose=True, render=False)
+            X_new, Y_new, _, _ = rollout(
+                env,
+                pilco=pilco,
+                timesteps=T,
+                verbose=True,
+                render=False
+            )
             new_data = True
-            X = np.vstack((X, X_new)); Y = np.vstack((Y, Y_new))
+            X = jnp.vstack((X, X_new))
+            Y = jnp.vstack((Y, Y_new))
             pilco.mgpr.set_data((X, Y))
             if overall_risk < (th/4):
-                pilco.mu.assign(0.75 * pilco.mu.numpy())
+                pilco.mu.assign(0.75 * pilco.mu)
 
         else:
-            X_new, Y_new,_,_ = rollout(env, pilco=pilco, timesteps=T, verbose=True, render=False)
-            print(m_p[:,0] - X_new[:,0])
-            print(m_p[:,2] - X_new[:,2])
+            X_new, Y_new, _, _ = rollout(
+                env,
+                pilco=pilco,
+                timesteps=T,
+                verbose=True,
+                render=False
+            )
+            print(m_p[:, 0] - X_new[:, 0])
+            print(m_p[:, 2] - X_new[:, 2])
             print("*********CHANGING***********")
             _, _, r = pilco.predict(m_init, S_init, T)
             print(r)
-            # to verify this actually changes, run the reward wrapper before and after on the same trajectory
+            # to verify this actually changes, run the reward wrapper before
+            # and after on the same trajectory
             pilco.mu.assign(1.5 * pilco.mu.numpy())
             _, _, r = pilco.predict(m_init, S_init, T)
             print(r)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     safe_cars()
